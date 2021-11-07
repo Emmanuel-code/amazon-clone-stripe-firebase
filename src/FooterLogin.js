40a2772d65dd4ac9a702ee83acb82a1f57620eed
Emmanuel-code <b.emma.j37@gmail.com>
2021-11-07 03:33:58 +0000

    first commit

---
+++ src/FooterLogin.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./FooterLogin.css";
+
+function FooterLogin() {
+  return (
+    <div className="footerLogin">
+      <hr />
+      <p className="centered">
+        Conditions of Use Privacy Notice Help © 2021 emmanuel jimah
+      </p>
+    </div>
+  );
+}
+
+export default FooterLogin;
